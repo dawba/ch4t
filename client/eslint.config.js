ae9a5169b848953d6e2ded7b1179da204b5d3d4e
dawba <dawid.bania1@gmail.com>
2024-04-17 01:50:09 +0200

    turn ban ts comment off

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -6,4 +6,10 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
+    // We can add or overwrite rules here:
+    {
+        rules: {
+            '@typescript-eslint/ban-ts-comment': "off"
+        }
+    }
 );
