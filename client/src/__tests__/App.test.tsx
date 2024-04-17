365032f6559c69a19f9edeaa90fa165cef9a09b1
dawba <dawid.bania1@gmail.com>
2024-04-17 02:39:54 +0200

    integrate vitest with jsdom

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,11 +1,8 @@
-// Imports
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-// To Test
 import App from '../App';
 
-// Tests
 describe('Renders main page correctly', async () => {
     it('Should render the page correctly', async () => {
         // Setup
