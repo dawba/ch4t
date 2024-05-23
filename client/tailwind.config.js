3ebb537ad605e762168f3d424320b5baf3de2817
Admswj <adam.suwaj@spinney.io>
2024-05-23 21:44:32 +0200

    update config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      colors: {
+        'primary-yellow': '#FBFF4A',
+        'background-gray': '#090909'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwind-plugin/expose-colors')({
+      extract: ['primary-yellow', 'background-gray']
+    })
+  ],
 };
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vitest/config';
 import react from "@vitejs/plugin-react-swc";
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
+// @ts-ignore
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        // @ts-ignore
+        react(),
+        // @ts-ignore
+        svgr({
+                svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
+                include: '**/*.svg',
+        }),
+    ],
     test: {
         // 👋 add the line below to add jsdom to vite
         environment: 'jsdom',
