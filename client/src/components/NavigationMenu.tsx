3d488ac4ff07493c9ee1977431e87ed6e9c7244f
Admswj <adam.suwaj@spinney.io>
2024-06-05 14:40:01 +0200

    Update nav menu item logic

--- client/src/components/NavigationMenu.tsx
+++ client/src/components/NavigationMenu.tsx
@@ -9,34 +9,36 @@ import { ReactComponent as AddChatIcon } from '../assets/nav_menu_icons/add_chat
 import { ReactComponent as AddChatIconActive } from '../assets/nav_menu_icons/add_chat_icon_active.svg';
 import { ReactComponent as SettingsIcon } from '../assets/nav_menu_icons/settings_icon.svg';
 import { ReactComponent as SettingsIconActive } from '../assets/nav_menu_icons/settings_icon_active.svg';
-import {useState} from "react";
 
 type SVGIcon = React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
 
-class MenuItem {
+export type MenuItem = 'DirectChats' | 'GroupChats' | 'AddChat' | 'Settings';
+class MenuIconItem {
+    menu: MenuItem
     icon: SVGIcon;
     activeIcon: SVGIcon;
 
-    private constructor(icon: SVGIcon, activeIcon: SVGIcon) {
+    private constructor(menu: MenuItem, icon: SVGIcon, activeIcon: SVGIcon) {
+        this.menu = menu;
         this.icon = icon;
         this.activeIcon = activeIcon;
     }
 
-    static DirectChats = new MenuItem(DirectChatsIcon, DirectChatsIconActive);
-    static GroupChats = new MenuItem(GroupChatsIcon, GroupChatsIconActive);
-    static AddChat = new MenuItem(AddChatIcon, AddChatIconActive);
-    static Settings = new MenuItem(SettingsIcon, SettingsIconActive);
+    static DirectChats = new MenuIconItem('DirectChats', DirectChatsIcon, DirectChatsIconActive, );
+    static GroupChats = new MenuIconItem('GroupChats', GroupChatsIcon, GroupChatsIconActive);
+    static AddChat = new MenuIconItem('AddChat', AddChatIcon, AddChatIconActive);
+    static Settings = new MenuIconItem('Settings', SettingsIcon, SettingsIconActive);
 
-    static allMenuItems = [
-        MenuItem.DirectChats,
-        MenuItem.GroupChats,
-        MenuItem.AddChat,
-        MenuItem.Settings
+    static allMenuIconItems = [
+        MenuIconItem.DirectChats,
+        MenuIconItem.GroupChats,
+        MenuIconItem.AddChat,
+        MenuIconItem.Settings
     ];
 }
 
 type NavigationButtonProps = {
-    menuItem: MenuItem
+    menuItem: MenuIconItem
     isActive: boolean;
     onClick: () => void;
 }
@@ -54,19 +56,22 @@ const NavigationButton = ({menuItem, isActive, onClick}: NavigationButtonProps) 
         </button>
     );
 }
-const NavigationMenu = () => {
-    const [activeItem, setActiveItem] = useState<MenuItem>(MenuItem.DirectChats);
+type NavigationMenuProps = {
+    activeItem: MenuItem;
+    setActiveItem: (item: MenuItem) => void;
+};
 
+const NavigationMenu = ({ activeItem, setActiveItem }: NavigationMenuProps) => {
     return (
         <div className="w-[8vw] h-full bg-primary-gray flex flex-col items-center mr-1">
             <Logo className="w-24 h-24 mx-auto mt-5 mb-12"/>
             <div className="flex flex-col items-center h-full">
-                {MenuItem.allMenuItems.map((item, index) => (
-                    <div className={'mb-4' + (index === MenuItem.allMenuItems.length - 1 ? ' mt-auto' : '')}>
+                {MenuIconItem.allMenuIconItems.map((iconItem, index) => (
+                    <div className={'mb-4' + (index === MenuIconItem.allMenuIconItems.length - 1 ? ' mt-auto' : '')}>
                         <NavigationButton
-                            menuItem={item}
-                            isActive={activeItem === item}
-                            onClick={() => setActiveItem(item)} />
+                            menuItem={iconItem}
+                            isActive={activeItem === iconItem.menu}
+                            onClick={() => setActiveItem(iconItem.menu)} />
                     </div>
                 ))}
             </div>
