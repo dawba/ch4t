d27809d33b34a24e3eb4b590327b1eaf86bb0e7f
Admswj <adam.suwaj@spinney.io>
2024-06-12 00:02:29 +0200

    add settings menu and user provider

--- client/src/pages/MessagingPage.tsx
+++ client/src/pages/MessagingPage.tsx
@@ -1,16 +1,19 @@
 import React, {useState} from "react";
 import NavigationMenu, {MenuItem} from "../components/NavigationMenu.tsx";
 import ContextMenu from "../components/ContextMenu.tsx";
+import {UserProvider} from "../components/UserProvider.tsx";
 
 
 const MessagingPage: React.FC = () => {
     const [activeItem, setActiveItem] = useState<MenuItem>('DirectChats');
 
     return (
-        <div className="h-full w-full flex flex-row items-start">
-            <NavigationMenu activeItem={activeItem} setActiveItem={setActiveItem} />
-            <ContextMenu activeMenuItem={activeItem} />
-        </div>
+        <UserProvider>
+            <div className="h-full w-full flex flex-row items-start">
+                <NavigationMenu activeItem={activeItem} setActiveItem={setActiveItem}/>
+                <ContextMenu activeMenuItem={activeItem}/>
+            </div>
+        </UserProvider>
     );
 }
 
