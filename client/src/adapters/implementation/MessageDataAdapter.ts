d14ba1b9cd971f59462015b449e197b304819e99
dawba <dawid.bania1@gmail.com>
2024-06-12 00:40:30 +0200

    merge resolve + prettier rc config update

--- client/src/adapters/implementation/MessageDataAdapter.ts
+++ client/src/adapters/implementation/MessageDataAdapter.ts
@@ -2,7 +2,11 @@ import { IMessageAdapter } from '../interfaces/IMessageAdapter.ts'
 import { MessageTileProps } from '../../components/MessageList.tsx'
 
 export const MessageDataAdapter: IMessageAdapter = {
-  getMessages: (messageData: any[], currentUser, users): MessageTileProps[] => {
+  getMessages: (
+    messageData: Object[],
+    currentUser,
+    users
+  ): MessageTileProps[] => {
     return messageData.map((message) => {
       const sender = users.find((user) => user.id === message.sender)
       const messageSentByUser = message.sender === currentUser
