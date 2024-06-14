010541c815de108d44f4e9ab5a1b887f6dcf037b
Admswj <adam.suwaj@spinney.io>
2024-06-14 18:46:33 +0200

    apply cosmetic changes to settings

--- client/src/components/menus/SettingsView.tsx
+++ client/src/components/menus/SettingsView.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useContext } from 'react';
-import { ReactComponent as GalleryIcon } from '../assets/gallery_icon.svg';
-import placeholderPath from '../assets/pfp_placeholder.jpg';
-import { UserContext } from "./UserProvider.tsx";
+import { ReactComponent as GalleryIcon } from '../../assets/gallery_icon.svg';
+import placeholderPath from '../../assets/pfp_placeholder.jpg';
+import { UserContext } from "../UserProvider.tsx";
 
 const SettingsView = () => {
     const userContext = useContext(UserContext);
@@ -32,14 +32,14 @@ const SettingsView = () => {
     };
 
     return (
-        <div className="flex flex-col p-7">
+        <div className="w-full flex flex-col p-7">
             <label className="relative flex flex-col items-center self-center group">
                 <input
                     type="file"
                     className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
                     onChange={handleImageUpload}
                 />
-                <div className="w-32 h-32 rounded-full bg-gray-200 flex items-center justify-center overflow-hidden mt-8 relative border-2 border-primary-yellow">
+                <div className="w-32 h-32 rounded-full bg-gray-200 flex items-center justify-center overflow-hidden mt-6 relative border-2 border-primary-yellow">
                     <img src={pfp !== '' ? pfp : placeholderPath} alt="Profile" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-80 transition-opacity flex items-center justify-center">
                         <GalleryIcon className="w-8 h-8 text-white opacity-0 group-hover:opacity-100" />
