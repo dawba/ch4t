a08dacbdcbbde393939d30d0a1d90f2b62dc293e
Admswj <adam.suwaj@spinney.io>
2024-06-14 18:52:43 +0200

    remove redundant logic and move icon

--- client/src/components/menus/ChatTile.tsx
+++ client/src/components/menus/ChatTile.tsx
@@ -1,6 +1,6 @@
-import styles from '../styles/ChatTile.module.css';
-import { ReactComponent as ReadIcon } from '../assets/message_read.svg';
-import { ReactComponent as UnreadIcon } from '../assets/message_unread.svg';
+import styles from '../../styles/ChatTile.module.css';
+import { ReactComponent as ReadIcon } from '../../assets/message_read.svg';
+import { ReactComponent as UnreadIcon } from '../../assets/message_unread.svg';
 import { Chat } from '../../types/types.ts';
 
 const ChatTile = ({ chat }: { chat: Chat }) => {
@@ -12,14 +12,12 @@ const ChatTile = ({ chat }: { chat: Chat }) => {
     profilePicture,
   } = chat;
 
-  const messageToDisplay =
-    lastMessage.length > 25 ? lastMessage.slice(0, 25) + '...' : lastMessage;
   const StatusIcon = isLastMessageRead ? ReadIcon : UnreadIcon;
-  const lastSenderAndMessage = lastSender + ': ' + messageToDisplay;
+  const lastSenderAndMessage = lastSender + ': ' + lastMessage;
 
   return (
     <div
-      className={`h-auto w-full mx-2 flex items-center ${styles.messageTile}`}
+      className={`h-auto w-full flex items-center ${styles.messageTile}`}
     >
       <div className="w-12 h-12 flex-none items-center justify-center border-2 border-primary-yellow rounded-full overflow-hidden mr-4">
         <img
@@ -31,8 +29,8 @@ const ChatTile = ({ chat }: { chat: Chat }) => {
       <div className="flex flex-col grow truncate">
         <div className={styles.senderName}>{chatName}</div>
         <div className={styles.lastMessage}>{lastSenderAndMessage}</div>
-        <StatusIcon className={'w-4 h-4 flex-none mx-2'} />
       </div>
+      <StatusIcon className={'w-4 h-4 flex-none mx-2'} />
     </div>
   );
 };
