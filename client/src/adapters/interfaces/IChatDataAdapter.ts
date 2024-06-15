74b27ced628c37725c56fd524c82a19582c1ce48
Admswj <adam.suwaj@spinney.io>
2024-06-16 00:06:11 +0200

    update adapters

--- client/src/adapters/interfaces/IChatDataAdapter.ts
+++ client/src/adapters/interfaces/IChatDataAdapter.ts
@@ -1,5 +1,5 @@
 import { Chat, ChatData, ID } from '../../types/types.ts';
 
 export interface IChatDataAdapter {
-  getChats: (chatData: ChatData[], currentUserId: ID) => Chat[];
+  getChats: (chatData: ChatData[], currentUserId: ID) => Promise<Chat[]>;
 }
